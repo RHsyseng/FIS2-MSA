bd4226b079554ad0dc2f8cbd9edfa2c567e7aa4c
Jeremy Ary <jary@redhat.com>
2017-05-23 19:29:37 -0500

    adding 5s delay so status of paid can be seen in order history prior to shipped

--- Warehouse/src/main/java/com/redhat/refarch/microservices/warehouse/service/WarehouseService.java
+++ Warehouse/src/main/java/com/redhat/refarch/microservices/warehouse/service/WarehouseService.java
@@ -45,6 +45,8 @@ public class WarehouseService {
                 + "/orders/" + result.getOrderNumber());
         HttpPatch patch = new HttpPatch(uriBuilder.build());
         patch.setEntity(new StringEntity(jsonObject.toString(), ContentType.APPLICATION_JSON));
+        logInfo("Waiting 5 seconds to signify warehouse processing delay" + patch);
+        Thread.sleep(5000);
         logInfo("Executing " + patch);
         HttpResponse response = client.execute(patch);
         String responseString = EntityUtils.toString(response.getEntity());
